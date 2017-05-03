6a82782158b1b177c897f0957556947923cef653
Drew Palazzari <drewpalazzari@hotmail.com>
2017-05-02 18:51:59 -0600

    Minor style adjustments

--- app/components/Home.js
+++ app/components/Home.js
@@ -8,7 +8,7 @@ class Home extends React.Component{
         <img className='avatar' src="https://scontent.xx.fbcdn.net/v/t31.0-8/861271_10200590770874319_1434066940_o.jpg?oh=905c866a03b34296355750cc678d8a15&oe=598B9E07" />
         <p className='faq'>Coming from a background as a molecular lab technician, I am passionate about clean, well-tested code with great documentation that focuses on the customer&#39;s needs. I thrive in challenging environments where each day presents a problem to be solved and new ideas are encouraged. I&#39;m interested in working in a place that pushes employees to grow and allows their engineers to help shape the product. I love working on the entire stack, whether it&#39;s designing a database, building an API, or styling a page. I feel most comfortable with Rails, Ruby, and JavaScript, but am excited to work with any new language or framework.</p>
         <p className='faq'>For instance, as evidenced by this website, I am in the process of learning React.js.</p>
-        <h2>Relevant Links</h2>
+        <h3>Relevant Links</h3>
         <div className='relevant-links'>
           <a href="https://github.com/Dpalazzari" target="_blank"><img className='resources' src="https://image.flaticon.com/icons/svg/25/25231.svg" /></a>
           <a href="https://www.linkedin.com/in/drewpalazzari/" target="_blank"><img className='resources' src="https://image.flaticon.com/icons/svg/34/34405.svg" /></a>
